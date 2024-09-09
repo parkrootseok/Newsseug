193377561420024992e434180bf89e1a66d2c44c
mkkim <infinitelove367@gmail.com>
2024-09-09 10:45:53 +0900

    Chore: 필요한 라이브러리 설치

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,14 +4,18 @@ import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { lightTheme } from "./styles/theme";
 import "./styles/font.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
