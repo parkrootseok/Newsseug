9b68b4f7298493e95cf962da32842b36b1fd26c2
김주연 <juyun7908@naver.com>
2024-09-13 08:39:50 +0900

    feat: 공용 컴포넌트 카테고리 UI 구현(#4)

---
+++ frontend/src/components/subscribe/SubscribePressCard.tsx
@@ -0,0 +1,50 @@
+import { SubscribePressCardProps } from '@/types/subscribe';
+import styled from 'styled-components';
+import {
+  PressLogo,
+  LogoContainer,
+  PressName,
+  PressContainer,
+} from 'styles/subscribe-styles';
+
+function SubscribePressCard({
+  imgUrl,
+  pressName,
+  isActive,
+  isAllActive,
+  onClick,
+}: SubscribePressCardProps) {
+  return (
+    <Container isActive={isActive} isAllActive={isAllActive} onClick={onClick}>
+      <LogoContainer>
+        <PressLogo src={imgUrl} />
+      </LogoContainer>
+      <PressName>{pressName}</PressName>
+    </Container>
+  );
+}
+
+export default SubscribePressCard;
+
+const Container = styled(PressContainer)<{
+  isActive: boolean;
+  isAllActive: boolean;
+}>`
+  opacity: ${({ isActive, isAllActive }) =>
+    isActive || isAllActive ? 1 : 0.5};
+  background-color: ${({ isActive, isAllActive }) =>
+    isActive && !isAllActive ? '#dfdfdf52' : 'transparent'};
+
+  -webkit-transition:
+    opacity 0.4s ease-in-out,
+    background-color 0.4s ease-in-out;
+  -moz-transition:
+    opacity 0.4s ease-in-out,
+    background-color 0.4s ease-in-out;
+  -o-transition:
+    opacity 0.4s ease-in-out,
+    background-color 0.4s ease-in-out;
+  transition:
+    opacity 0.4s ease-in-out,
+    background-color 0.4s ease-in-out;
+`;
--- frontend/src/pages/AllSubscribes.tsx
+++ frontend/src/pages/AllSubscribes.tsx
@@ -1,8 +1,23 @@
+import SubscribeHeader from 'components/subscribe/SubscribeHeader';
+import SubLayout from 'components/common/SubLayout';
+import PressCardList from 'components/subscribe/PressCardList';
+
+const subscribeNumber = 6;
+
 function AllSubscribes() {
   return (
-    <div>
-      <h1>AllSubscribes</h1>
-    </div>
+    <SubLayout>
+      <div>내 구독목록</div>
+      <div>
+        <SubscribeHeader
+          title="구독한 언론사"
+          subscribeNumber={subscribeNumber}
+          variant="all"
+        />
+        <PressCardList />
+        <SubscribeHeader title="전체 언론사" variant="all" />
+      </div>
+    </SubLayout>
   );
 }
 
