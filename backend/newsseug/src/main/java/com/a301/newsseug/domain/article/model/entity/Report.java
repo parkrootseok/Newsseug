f9b953193e0cc821d2ee9102cf38d6a68f25d9ff
박상후 <tkdgn407@naver.com>
2024-09-19 09:59:06 +0900

    09/19 09:56 feature/13 articles entity => develop (#13)

---
+++ backend/newsseug/src/main/java/com/a301/newsseug/domain/article/model/entity/Report.java
@@ -0,0 +1,39 @@
+package com.a301.newsseug.domain.article.model.entity;
+
+import com.a301.newsseug.domain.article.model.entity.type.ReportType;
+import com.a301.newsseug.global.util.ClockUtil;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "reports")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reportId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReportType type;
+
+    @CreatedDate
+    @Column(updatable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onPrePersist() {
+        this.createdAt = ClockUtil.getLocalDateTime();
+    }
+}
---
+++ backend/newsseug/src/main/java/com/a301/newsseug/domain/member/model/entity/History.java
@@ -0,0 +1,42 @@
+package com.a301.newsseug.domain.member.model.entity;
+
+import com.a301.newsseug.domain.article.model.entity.Article;
+import com.a301.newsseug.global.util.ClockUtil;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "histories")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class History {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long historyId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+
+    @Column(nullable = false)
+    private int playTime;
+
+    @CreatedDate
+    @Column(updatable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onPrePersist() {
+        this.createdAt = ClockUtil.getLocalDateTime();
+    }
+}
