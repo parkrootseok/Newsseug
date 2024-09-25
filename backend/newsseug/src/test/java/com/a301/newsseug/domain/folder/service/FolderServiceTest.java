f99f470858cd8ed2cb9f8027d3f2ec93336d0178
parkrootseok <parkrootseok@gmail.com>
2024-09-25 16:01:10 +0900

    test: 폴더 생성 (#51)

--- backend/newsseug/src/test/java/com/a301/newsseug/domain/folder/service/FolderServiceTest.java
+++ backend/newsseug/src/test/java/com/a301/newsseug/domain/folder/service/FolderServiceTest.java
@@ -3,6 +3,7 @@ package com.a301.newsseug.domain.folder.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.tuple;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -10,7 +11,8 @@ import static org.mockito.Mockito.when;
 import com.a301.newsseug.domain.auth.model.entity.CustomUserDetails;
 import com.a301.newsseug.domain.bookmark.repository.BookmarkRepository;
 import com.a301.newsseug.domain.folder.exception.InaccessibleFolderException;
-import com.a301.newsseug.domain.folder.factory.FolderFactory;
+import com.a301.newsseug.domain.folder.factory.entity.FolderFactory;
+import com.a301.newsseug.domain.folder.factory.fixtures.FolderFixtures;
 import com.a301.newsseug.domain.folder.model.dto.FolderDto;
 import com.a301.newsseug.domain.folder.model.dto.response.GetFolderResponse;
 import com.a301.newsseug.domain.folder.model.dto.response.ListFolderResponse;
@@ -55,10 +57,10 @@ class FolderServiceTest {
     }
 
     @Test
-    @DisplayName("폴더 조회")
+    @DisplayName("폴더 조회[성공]")
     void getFolder() {
 
-        Folder folder = FolderFactory.folder(1L, "folder1");
+        Folder folder = FolderFactory.folder(1L);
 
         // Given
         given(folderRepository.findByFolderIdAndMemberAndStatus(folder.getFolderId(), loginMember, ActivateStatus.ACTIVE))
@@ -80,11 +82,11 @@ class FolderServiceTest {
     }
 
     @Test
-    @DisplayName("폴더 조회[접근할 수 없는 폴더]")
+    @DisplayName("폴더 조회[실패 - 소유자가 아님]")
     void getFolderInaccessibleFolder() {
 
         // Given
-        Folder folder = FolderFactory.folder(1L, "folder1");
+        Folder folder = FolderFactory.folder(1L);
         when(userDetails.getMember()).thenReturn(loginMember);
         when(folderRepository.findByFolderIdAndMemberAndStatus(folder.getFolderId(), loginMember, ActivateStatus.ACTIVE))
                 .thenReturn(Optional.empty());
@@ -97,14 +99,14 @@ class FolderServiceTest {
 
 
     @Test
-    @DisplayName("사용자 폴더 목록 조회")
+    @DisplayName("목록 조회[성공]")
     void getFolderByMember() {
 
         // Given
         given(folderRepository.findAllByMember(loginMember)).willReturn(
                 List.of(
-                        FolderFactory.folder(1L, "folder1"),
-                        FolderFactory.folder(2L, "folder2")
+                        FolderFactory.folder(1L),
+                        FolderFactory.folder(2L)
                 ));
 
         // When
@@ -117,9 +119,25 @@ class FolderServiceTest {
         assertThat(response.folders())
                 .extracting(FolderDto::id, FolderDto::name, FolderDto::articleCount)
                 .containsExactlyInAnyOrder(
-                        tuple(1L, "folder1", 0L),
-                        tuple(2L, "folder2", 0L)
+                        tuple(1L, FolderFixtures.name, 0L),
+                        tuple(2L, FolderFixtures.name, 0L)
                 );
+
+    }
+
+    @Test
+    @DisplayName("폴더 생성 [성공]")
+    void createFolder() {
+
+        // Given
+        Folder folder = FolderFactory.folder(1L);
+
+        // When
+        folderService.createFolder(userDetails, folder.getName());
+
+        // Then
+        verify(folderRepository).save(any(Folder.class));
+
 
     }
 
