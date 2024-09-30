7206a052aa5adfa290f6b2cfadbf200b2a3f1dfd
김주연 <juyun7908@naver.com>
2024-09-30 13:10:55 +0900

    언론사 별 기사 조회 API 연동

--- frontend/src/apis/subscribe.ts
+++ frontend/src/apis/subscribe.ts
@@ -1,8 +1,36 @@
 import api from 'apis/commonApi';
 import { isAxiosError } from 'axios';
 import { PressBasic } from 'types/api/press';
+import { PageType } from 'types/api/article';
 
 const MEMBER_URL = '/api/v1/members';
+
+/**
+ * IMP : 언론사 별 기사 조회를 위한 API
+ */
+export const getPressArticleList = async (
+  pressId?: number | null,
+  pageNumber?: number,
+  filter?: string,
+): Promise<PageType> => {
+  try {
+    const url = pressId
+      ? `/api/v1/articles/press/${pressId}`
+      : '/api/v1/articles/press';
+
+    const response = await api.get(url, {
+      params: { pageNumber, filter },
+    });
+    console.log(response.data.data);
+    return response.data.data;
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      if (error.response?.status === 404) {
+        throw new Error('Not Found');
+      } else throw error;
+    } else throw error;
+  }
+};
 
 /**
  * IMP : 언론사 구독 목록 조회를 위한 API
@@ -10,7 +38,6 @@ const MEMBER_URL = '/api/v1/members';
 export const getSubscribedPressList = async (): Promise<PressBasic[]> => {
   try {
     const response = await api.get(`${MEMBER_URL}/press`);
-    console.log(response.data);
     return response.data.data.press;
   } catch (error: unknown) {
     if (isAxiosError(error)) {
@@ -57,7 +84,7 @@ export const subscribePress = async (pressId: number) => {
  */
 export const unsubscribePress = async (pressId: number) => {
   try {
-    await api.put(`${MEMBER_URL}/press/${pressId}`);
+    await api.delete(`${MEMBER_URL}/press/${pressId}`);
   } catch (error: unknown) {
     if (isAxiosError(error)) {
       if (error.response?.status === 404) {
