7f6dc6ed3d1e08083dcec733f2e39bfe9821933b
박찬규 <cham9906@naver.com>
2024-10-02 11:01:34 +0900

    refactor: 응답 dto 수정

--- backend/newsseug/src/main/java/com/a301/newsseug/domain/interaction/service/HistoryService.java
+++ backend/newsseug/src/main/java/com/a301/newsseug/domain/interaction/service/HistoryService.java
@@ -1,11 +1,13 @@
 package com.a301.newsseug.domain.interaction.service;
 
+import java.util.List;
+
 import com.a301.newsseug.domain.auth.model.entity.CustomUserDetails;
-import com.a301.newsseug.domain.interaction.model.dto.response.ListHistoryResponse;
+import com.a301.newsseug.domain.interaction.model.dto.HistoryDto;
 import com.a301.newsseug.global.model.dto.SlicedResponse;
 
 public interface HistoryService {
 
-	SlicedResponse<ListHistoryResponse> getHistories(CustomUserDetails userDetails, int page);
+	SlicedResponse<List<HistoryDto>> getHistories(CustomUserDetails userDetails, int page);
 
 }
