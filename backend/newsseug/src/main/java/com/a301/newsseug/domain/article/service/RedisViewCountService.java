ec558e6126f46d43f1d56305d5eb63e23a981864
박상후 <tkdgn407@naver.com>
2024-10-07 09:50:51 +0900

    remove: viewCountService (#95)

--- backend/newsseug/src/main/java/com/a301/newsseug/domain/article/service/RedisViewCountService.java
+++
@@ -1,8 +0,0 @@
-package com.a301.newsseug.domain.article.service;
-
-import com.a301.newsseug.external.redis.service.RedisHashService;
-import com.a301.newsseug.external.redis.service.RedisKeyValueService;
-
-public interface RedisViewCountService extends RedisHashService<String, String, Integer> {
-
-}
