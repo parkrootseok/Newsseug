0b6614521064a1ba4376e0b1b76ea4cc6894aa2d
김주연 <juyun7908@naver.com>
2024-10-07 13:51:30 +0900

    기사 리스트 페이지에서 숏폼 연결

--- frontend/src/hooks/useStoreArticleDispatch.ts
+++ frontend/src/hooks/useStoreArticleDispatch.ts
@@ -5,25 +5,19 @@ import {
   setActviePress,
   setArticleFrom,
   setArticleIds,
+  setFolderId,
   setSliceDetail,
 } from '../redux/articleSlice';
 import { Category, SliceDetails } from 'types/api/article';
 import { ArticleListCardProps } from 'types/common/common';
-
-/**
- * Articles 관련 Redux 상태 업데이트를 처리하는 훅
- * @param articleList - 불러온 기사 리스트
- * @param sliceDetails - 페이지네이션 슬라이스 정보
- * @param sectionType - 섹션 타입
- * @param activeCategory - 활성화된 카테고리
- */
 
 function useStoreArticleDispatch(
   articleList: ArticleListCardProps[],
   sliceDetails: SliceDetails | {},
   articleFrom: string,
   activeCategory?: string,
-  activePress?: string,
+  activePress?: number | null,
+  folerId?: number,
 ) {
   const dispatch = useDispatch();
 
@@ -34,8 +28,17 @@ function useStoreArticleDispatch(
     dispatch(
       setActiveCategory(Category[activeCategory as keyof typeof Category]),
     );
-    dispatch(setActviePress(activePress || null));
-  }, [articleList, sliceDetails, articleFrom, activeCategory, dispatch]);
+    dispatch(setActviePress(activePress ?? null));
+    dispatch(setFolderId(folerId ?? null));
+  }, [
+    articleList,
+    sliceDetails,
+    articleFrom,
+    activeCategory,
+    activePress,
+    folerId,
+    dispatch,
+  ]);
 }
 
 export default useStoreArticleDispatch;
--- frontend/src/pages/History.tsx
+++ frontend/src/pages/History.tsx
@@ -3,13 +3,22 @@ import { PageType } from 'types/api/article';
 import useContentsFetch from 'hooks/useContentsFetch';
 import SubLayout from 'components/common/SubLayout';
 import ArticleListCardGroup from 'components/common/ArticleListCardGroup';
+import useStoreArticleDispatch from 'hooks/useStoreArticleDispatch';
 
 function History() {
-  const { articleList, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useContentsFetch<PageType>({
-      queryKey: ['myPageHistory'],
-      fetchData: getMemberHistoryList,
-    });
+  const {
+    articleList,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    sliceDetails,
+  } = useContentsFetch<PageType>({
+    queryKey: ['myPageHistory'],
+    fetchData: getMemberHistoryList,
+  });
+
+  useStoreArticleDispatch(articleList, sliceDetails, 'history');
+
   return (
     <SubLayout>
       <span>내 시청 기록</span>
