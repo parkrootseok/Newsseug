603e28cdb214ca0a7ae46c4ff82741e7bd7ec2be
박근석 <parkrootseok@gmail.com>
2024-10-08 20:08:11 +0900

    feat: WebMvcConfig
    - Resolver 등록

--- backend/newsseug/src/main/java/com/a301/newsseug/global/config/WebMvcConfig.java
+++ backend/newsseug/src/main/java/com/a301/newsseug/global/config/WebMvcConfig.java
@@ -1,8 +1,17 @@
 package com.a301.newsseug.global.config;
 
+import com.a301.newsseug.global.resolver.NullableUserDetailsResolver;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new NullableUserDetailsResolver());
+    }
+
 }
