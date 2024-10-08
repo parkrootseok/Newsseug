48fa406c478ef0c1d49f24397ba1af2767100468
박찬규 <cham9906@naver.com>
2024-10-08 17:23:48 +0900

    10/08 17:22 feature/123-embedd-api => develop

--- backend/article_creator/article.py
+++ backend/article_creator/article.py
@@ -12,7 +12,7 @@ import base64
 from PIL import Image
 import cv2
 
-from config import config
+from config import config, log_format
 
 logger = logging.getLogger('article-logger')
 logger.setLevel(logging.INFO)  # 로그 레벨 설정
@@ -22,7 +22,7 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)  # 핸들러 레벨 설정
 
 # 로그 포맷 설정
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(log_format)
 console_handler.setFormatter(formatter)
 
 # 핸들러를 로거에 추가
