c21ae794cac4ad3c388b2f7d00bf242ae2e6aa54
박찬규 <cham9906@naver.com>
2024-10-10 19:06:22 +0900

    fix: 연령별 기사 오류 해결

--- backend/newsseug/src/main/java/com/a301/newsseug/domain/article/repository/ArticleRepository.java
+++ backend/newsseug/src/main/java/com/a301/newsseug/domain/article/repository/ArticleRepository.java
@@ -47,9 +47,9 @@ public interface ArticleRepository extends JpaRepository<Article, Long>, Article
 
     @Query("SELECT a " +
         "FROM Article a " +
-        "JOIN BirthYearViewCount b ON b.article = a WHERE YEAR(CURRENT_DATE) - b.birthYear " +
-        "BETWEEN :ageBegin AND :ageEnd " +
-        "WHERE a.category = :category " +
+        "JOIN BirthYearViewCount b ON b.article = a " +
+        "WHERE YEAR(CURRENT_DATE) - b.birthYear " +
+        "BETWEEN :ageBegin AND :ageEnd AND a.category = :category " +
         "GROUP BY a ORDER BY SUM(b.viewCount) DESC")
     Slice<Article> findAllByBirthYearOrderByViewCountFiltered(@Param("ageBegin") Integer ageBegin, @Param("ageEnd") Integer ageEnd, @Param("category") CategoryType category, Pageable pageable);
 }
