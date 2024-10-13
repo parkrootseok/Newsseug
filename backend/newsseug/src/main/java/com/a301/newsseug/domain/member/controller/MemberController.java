e66b6aed5763aa649b7df1ac43e1af83aa1df825
박근석 <parkrootseok@gmail.com>
2024-10-13 17:54:14 +0900

    refactor: 시청 기록 조

--- backend/newsseug/src/main/java/com/a301/newsseug/domain/member/controller/MemberController.java
+++ backend/newsseug/src/main/java/com/a301/newsseug/domain/member/controller/MemberController.java
@@ -2,28 +2,20 @@ package com.a301.newsseug.domain.member.controller;
 
 import com.a301.newsseug.domain.auth.model.entity.CustomUserDetails;
 import com.a301.newsseug.domain.member.model.dto.request.UpdateMemberRequest;
-import com.a301.newsseug.domain.member.model.dto.response.GetMemberFolderResponse;
 import com.a301.newsseug.domain.member.model.dto.response.GetMemberResponse;
 import com.a301.newsseug.domain.member.service.MemberService;
-import com.a301.newsseug.domain.press.model.dto.response.GetPressResponse;
 import com.a301.newsseug.global.model.dto.Result;
-import com.a301.newsseug.global.model.dto.SlicedResponse;
 import com.a301.newsseug.global.util.ResponseUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "멤버 관련 API")
