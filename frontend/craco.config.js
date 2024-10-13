80152aa4750b9ef07b677da61df03ff97bac1934
Hwarang <keyein123@gmail.com>
2024-10-13 19:06:57 +0900

    🚑 hotfix

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,19 +1,14 @@
-export function devServer(devServerConfig) {
-  return {
-    ...devServerConfig,
-    port: 3000, //
-    liveReload: true,
-    host: '0.0.0.0',
-    allowedHosts: 'all',
-    open: true,
-    compress: true,
-    client: {
-      overlay: true,
-      webSocketURL: {
-        hostname: '0.0.0.0',
-        port: '80',
-        pathname: '/ws',
-      },
-    },
-  };
-}
+// craco.config.js
+module.exports = {
+  devServer: (devServerConfig) => {
+    devServerConfig.port = 3000;
+    devServerConfig.host = '0.0.0.0';
+    devServerConfig.allowedHosts = 'all';
+    devServerConfig.open = true;
+    devServerConfig.compress = true;
+    devServerConfig.hot = false; // Hot Module Replacement 비활성화
+    devServerConfig.liveReload = false; // 라이브 리로드 비활성화
+
+    return devServerConfig;
+  },
+};
