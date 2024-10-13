dddac39db163317309a2a03a5470935e1750a391
Hwarang <keyein123@gmail.com>
2024-10-14 00:25:57 +0900

    🐛 logout의 처리를 고침, 에러 표시를 배포 중에 없앰, Image Cropper UI 개선

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import Router from 'Router';
 import GlobalStyle from 'styles/global-styles';
 import { Helmet } from 'react-helmet-async';
 import { useTheme } from 'styled-components';
-import { ReactQueryDevtools } from 'react-query/devtools';
+// import { ReactQueryDevtools } from 'react-query/devtools';
 
 function App() {
   const theme = useTheme();
