e74ba0132c7e44ad44d89915adedc7937a36d607
박근석 <parkrootseok@gmail.com>
2024-10-21 21:02:44 +0900

    remove: 불필요한 파일 삭제

--- etc/오화랑/code/src_240827_ReactBasic/components/DeleteContentButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Trash2 } from 'lucide-react';
-
-export default function DeleteContentButton() {
-  return (
-    <button>
-      <Trash2 color='gray' size={40} />
-    </button>
-  );
-}
--- etc/오화랑/code/src_240827_ReactBasic/types/content.ts
+++
@@ -1,7 +0,0 @@
-export interface Content {
-  _id: number;
-  contentTitle: string;
-  description: string;
-  duration: number;
-  isMade: boolean;
-}
